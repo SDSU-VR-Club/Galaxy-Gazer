66289b0ee92aa3b100aa0632e019bbc1bfb53ff6
Kainkun <kainkun1000@gmail.com>
2018-12-02 00:40:38 -0800

    things should hypothetically be better

--- Assets/scripts/SpawnStarsCartesian2.cs
+++ Assets/scripts/SpawnStarsCartesian2.cs
@@ -1,6 +1,7 @@
 ﻿﻿using UnityEngine;
 using System.Collections;
 
+//[ExecuteInEditMode]
 public class SpawnStarsCartesian2 : MonoBehaviour {
 
     [Header("CSV file of star data")]
@@ -22,23 +23,29 @@ public class SpawnStarsCartesian2 : MonoBehaviour {
     [Space(10)]
 
     [Header("Column of the star's color")]
-    public bool HexadecimalColor;
+    public bool UseColor;
     public int RcolorColumn;
     public int GcolorColumn;
     public int BcolorColumn;
+    public bool UseHexadecimalColor;
+    public int HexColumn;
     [Space(10)]
 
     [Header("Star prefab")]
     public GameObject DefaultStar;
     string[] data;
 
-	void Start ()
+
+    
+
+        void Start ()
 	{
 
         data = starData.text.Split(new char[] { '\n' });
 
         for(int i = 1; i < data.Length; i++)
         {
+
             float x = 0;
             float y = 0;
             float z = 0;
@@ -51,12 +58,17 @@ public class SpawnStarsCartesian2 : MonoBehaviour {
             Color hexcolor;
 
             string[] row = data[i].Split(new char[] { ',' });
+
+
             //makestar
             GameObject tempStar = Instantiate(DefaultStar);
             //make star child
             tempStar.transform.parent = transform;
             //name star
-            tempStar.name = row[NameColumn - 1];
+            if (row[NameColumn - 1] != "")
+                tempStar.name = row[NameColumn - 1];
+            else
+                tempStar.name = "Nameless";
             //star position
             float.TryParse(row[Xcolumn - 1], out x);
             float.TryParse(row[Ycolumn - 1], out y);
@@ -66,28 +78,52 @@ public class SpawnStarsCartesian2 : MonoBehaviour {
             float.TryParse(row[DiamaterColumn - 1], out d);
             tempStar.transform.localScale = new Vector3(d, d, d);
             //star color
-            Material starMaterial = new Material(Shader.Find("Standard"));
-            tempStar.GetComponent<Renderer>().material = starMaterial;
-            if (HexadecimalColor)
+            if (UseColor)
             {
-                ColorUtility.TryParseHtmlString(row[RcolorColumn - 1], out hexcolor);
-                starMaterial.SetColor("_Color", hexcolor);
+                if (tempStar.GetComponent<Renderer>().material.name == "Default-Material (Instance)")
+                {
+                    Material starMaterial = new Material(Shader.Find("Standard"));
+                    tempStar.GetComponent<Renderer>().material = starMaterial;
+                    if (UseHexadecimalColor)
+                    {
+                        ColorUtility.TryParseHtmlString("#" + row[HexColumn - 1], out hexcolor);
+                        starMaterial.SetColor("_Color", hexcolor);
+                        starMaterial.SetColor("_EmissionColor", hexcolor);
+                    }
+                    else
+                    {
+                        float.TryParse(row[RcolorColumn - 1], out r);
+                        float.TryParse(row[GcolorColumn - 1], out g);
+                        float.TryParse(row[BcolorColumn - 1], out b);
+                        starMaterial.SetColor("_Color", new Color(r, g, b));
+                        starMaterial.SetColor("_EmissionColor", new Color(r, g, b));
+                    }
+                }
+                else
+                {
+                    if (UseHexadecimalColor)
+                    {
+                        ColorUtility.TryParseHtmlString("#" + row[HexColumn - 1], out hexcolor);
+                        tempStar.GetComponent<Renderer>().material.SetColor("_Color", hexcolor);
+                        tempStar.GetComponent<Renderer>().material.SetColor("_EmissionColor", hexcolor);
+                    }
+                    else
+                    {
+                        float.TryParse(row[RcolorColumn - 1], out r);
+                        float.TryParse(row[GcolorColumn - 1], out g);
+                        float.TryParse(row[BcolorColumn - 1], out b);
+                        tempStar.GetComponent<Renderer>().material.SetColor("_Color", new Color(r, g, b));
+                        tempStar.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(r, g, b));
+                    }
+                }
             }
-            else
-            {
-                float.TryParse(row[RcolorColumn - 1], out r);
-                float.TryParse(row[GcolorColumn - 1], out g);
-                float.TryParse(row[BcolorColumn - 1], out b);
-                starMaterial.SetColor("_Color", new Color(r, g, b));
-            }
-
 
         }
 	}
-	
 
-	void Update ()
+    
+    void Update ()
 	{
-		
+
 	}
 }
