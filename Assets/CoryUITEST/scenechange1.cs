802477d27ca88ec03daf76bc267e536e242c0424
Carter <andrewscart@gmail.com>
2019-02-01 11:56:55 -0800

    last minute

---
+++ Assets/CoryUITEST/scenechange1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class scenechange1 : MonoBehaviour {
+
+	public void changeToScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+	
+	
+}
