dfa3188f92166708303bc3f30babaf0450debbdb
Cory Clemens <44911328+coryclemens@users.noreply.github.com>
2019-02-01 11:03:02 -0800

    UI Test scene and Menu scene

---
+++ CoryUITEST/coryGameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class coryGameManager : MonoBehaviour
+{
+    public GameObject finalUI;
+    public TextMeshProUGUI finalText;
+    public GameObject reset;
+    public GameObject menu;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver(int score)
+    {
+        finalUI.SetActive(true);
+        finalText.SetText(score.ToString());
+        reset.SetActive(true);
+        menu.SetActive(true);
+
+    }
+}
