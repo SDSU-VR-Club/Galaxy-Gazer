f5955dcda922bc628d3d056a73b32c04d3ce235b
Kainkun <kainkun1000@gmail.com>
2019-02-08 19:43:36 -0800

    made scene OrbitTrails
    you orbit the sun(i should have put one in) the stars move. the stars have trails. all the stars are the same size regardless of distance.

---
+++ Assets/ScaleByDistance.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleByDistance : MonoBehaviour {
+
+    public float scale;
+    GameObject cam;
+    float dist;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        dist = Vector3.Distance(gameObject.transform.position, cam.transform.position);
+        float i = dist / Mathf.Pow(scale,-1) / 10;
+        transform.localScale = new Vector3(i, i, i);
+        GetComponent<TrailRenderer>().startWidth = i;
+	}
+}
---
+++ Assets/orbit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour {
+
+    float x;
+    float z;
+    public float Speed;
+    public float Xwidth;
+    public float Zwidth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        x = Mathf.Sin(Time.time * Speed) * Xwidth;
+        z = Mathf.Cos(Time.time * Speed) * Zwidth;
+
+        transform.position = new Vector3(x, 0, z);
+	}
+}
