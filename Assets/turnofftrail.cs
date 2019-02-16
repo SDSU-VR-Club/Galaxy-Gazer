208c55356ed5b34fa951a8f16e7b77842658cbf0
Vanessa Chammas <vvanessachammas@gmail.com>
2019-02-15 20:24:31 -0800

    turned off trails
    i hope it works!

---
+++ Assets/turnofftrail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnofftrail : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    int count = 0;
+    void Update()
+    {
+       
+        if(count == 0 && Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            GetComponent<TrailRenderer>().enabled = false;
+            count = 1;
+        }
+        else if(count == 1 && Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)){
+            GetComponent<TrailRenderer>().enabled = true;
+            count = 0;
+        }
+    }
+}
