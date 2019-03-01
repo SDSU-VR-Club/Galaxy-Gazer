1988b3d7209883ce9882d688afa6928b84ddcc1e
Carter <andrewscart@gmail.com>
2019-02-28 22:09:07 -0800

    smooth scene transitions
    fixed part 2 angle tool

--- Assets/scripts/StarBehavior.cs
+++ Assets/scripts/StarBehavior.cs
@@ -2,68 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 //see Carter for more info
-public class StarBehavior : MonoBehaviour {
-    //has the star been correctly selected
-    bool finished = false;
-    //has the star been falsely selected (only true for limited time)
-    bool failing = false;
-    Color originalColor;
-	// Use this for initialization
-	void Start () {
-        //so we can get back to the color we started at when we highlight
-        originalColor = GetComponent<SpriteRenderer>().color;
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        //if 
-        //dehighlight();
-	}
-    public void select()
+namespace Valve.VR.InteractionSystem
+{
+    public class StarBehavior : MonoBehaviour
     {
-        if (!finished&&!failing)
+        //has the star been correctly selected
+        bool finished = false;
+        //has the star been falsely selected (only true for limited time)
+        bool failing = false;
+        Color originalColor;
+        // Use this for initialization
+        void Start()
         {
-            FindObjectOfType<StarManager>().checkClosest(this);
-            
+            //so we can get back to the color we started at when we highlight
+            originalColor = GetComponent<SpriteRenderer>().color;
+
         }
-    }
-    //called in the event a star is successfully selected
-    public void success()
-    {
-        GetComponent<SpriteRenderer>().color=Color.green;
-        finished = true;
-    }
-    //turns the star red for 2 sec and prevents it from being selected again rapidly
-    public void fail()
-    {
-        
-        StartCoroutine(red());
-        
-    }
-    private IEnumerator red()
-    {
-        failing = true;
-        GetComponent<SpriteRenderer>().color = Color.red;
-        yield return new WaitForSeconds(2);
-        GetComponent<SpriteRenderer>().color = originalColor;
-        failing = false;
-    }
-    //called when a star is hovered over
-    public void highlight()
-    {
-        //print("The quick brown fox");
-        if (!finished && !failing)
+
+        // Update is called once per frame
+        void Update()
         {
-            GetComponent<SpriteRenderer>().color = Color.blue;
+            //if 
+            //dehighlight();
         }
-    }
-    //called every frame to counteract highlight
-    public void dehighlight()
-    {
-        if (!finished && !failing)
+        public void select()
         {
+            if (!finished && !failing)
+            {
+                FindObjectOfType<StarManager>().checkClosest(this);
+
+            }
+        }
+        //called in the event a star is successfully selected
+        public void success()
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+            finished = true;
+        }
+        //turns the star red for 2 sec and prevents it from being selected again rapidly
+        public void fail()
+        {
+
+            StartCoroutine(red());
+
+        }
+        private IEnumerator red()
+        {
+            failing = true;
+            GetComponent<SpriteRenderer>().color = Color.red;
+            yield return new WaitForSeconds(2);
             GetComponent<SpriteRenderer>().color = originalColor;
+            failing = false;
+        }
+        //called when a star is hovered over
+        public void highlight()
+        {
+            //print("The quick brown fox");
+            if (!finished && !failing)
+            {
+                GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+        }
+        //called every frame to counteract highlight
+        public void dehighlight()
+        {
+            if (!finished && !failing)
+            {
+                GetComponent<SpriteRenderer>().color = originalColor;
+            }
         }
     }
 }
--- Assets/scripts/StarManager.cs
+++ Assets/scripts/StarManager.cs
@@ -1,92 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 //see Carter for more info
-public class StarManager : MonoBehaviour {
-    //poulated on the first selection with all stars in the scene
-    List<Transform> stars;
-    public float score = 0;
-    public AudioSource badSound;
-    public AudioSource goodSound;
-    public GameObject currentConstellation;
-    public GameObject[] constellations;
-    int currentIndex = 0;
-    public Transform SpawnPosition;
-    // Use this for initialization
-    void Start () {
-       
-	}
-	public void Begin()
-    {
-        var tmp = Instantiate(constellations[currentIndex++]);
-        tmp.transform.position = SpawnPosition.position;
-        currentConstellation = tmp;
-    }
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Valve.VR.InteractionSystem
+{
+    public class StarManager : MonoBehaviour {
+        //poulated on the first selection with all stars in the scene
+        List<Transform> stars;
+        public float score = 0;
+        public AudioSource badSound;
+        public AudioSource goodSound;
+        public GameObject currentConstellation;
+        public GameObject[] constellations;
+        int currentIndex = 0;
+        public Transform SpawnPosition;
+        // Use this for initialization
+        void Start() {
 
-    public void checkClosest(StarBehavior starToCheck)
-    {
-        //initialize list of star transforms
-        if (stars == null) {
-            stars = new List<Transform>();
-            var starbehaviors = FindObjectsOfType<StarBehavior>();
-            foreach (StarBehavior a in starbehaviors)
-                stars.Add(a.transform);
+        }
+        public void Begin()
+        {
+            var tmp = Instantiate(constellations[currentIndex++]);
+            tmp.transform.position = SpawnPosition.position;
+            currentConstellation = tmp;
+        }
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void checkClosest(StarBehavior starToCheck)
+        {
+            //initialize list of star transforms
+            if (stars == null) {
+                stars = new List<Transform>();
+                var starbehaviors = FindObjectsOfType<StarBehavior>();
+                foreach (StarBehavior a in starbehaviors)
+                    stars.Add(a.transform);
+            }
+            //sort for the closest star
+            float lowest = Mathf.Infinity;
+            StarBehavior closestStar = null;
+            foreach (Transform a in stars)
+            {
+                var dist = Vector3.Distance(Camera.main.transform.position, a.position);
+                if (dist < lowest)
+                {
+                    lowest = dist;
+                    closestStar = a.GetComponent<StarBehavior>();
                 }
-        //sort for the closest star
-        float lowest = Mathf.Infinity;
-        StarBehavior closestStar=null;
-        foreach(Transform a in stars)
-        {
-            var dist = Vector3.Distance(Camera.main.transform.position, a.position);
-            if (dist < lowest)
-            {
-                lowest = dist;
-                closestStar = a.GetComponent<StarBehavior>();
             }
-        }
-        //compare closest star with user selection to determine success
-        if (closestStar==starToCheck)
-        {
-            //in the event of a success
-            starToCheck.success();
-            goodSound.Play();
-            stars.Remove(closestStar.transform);
-            if (stars.Count == 0)
+            //compare closest star with user selection to determine success
+            if (closestStar == starToCheck)
             {
-                GetComponent<UIManager>().nextUI((int)score);
-                nextConstellation();
+                //in the event of a success
+                starToCheck.success();
+                goodSound.Play();
+                stars.Remove(closestStar.transform);
+                if (stars.Count == 0)
+                {
+                    GetComponent<UIManager>().nextUI((int)score);
+                    nextConstellation();
+                }
+                return;
+
             }
-            return;
+            //in the event of a failure
+            starToCheck.fail();
+            badSound.Play();
+            score++;
 
         }
-        //in the event of a failure
-        starToCheck.fail();
-        badSound.Play();
-        score++;
-
-    }
-    public void nextConstellation()
-    {
-        //score screen pops up
+        public void nextConstellation()
+        {
+            //score screen pops up
 
 
-        StartCoroutine(cycleConstellation());
-    }
-    private IEnumerator cycleConstellation()
-    {
-        score = 0;
-        Destroy(currentConstellation);
-        stars = null;
-        yield return new WaitForSeconds(3);
-        if (currentIndex == constellations.Length - 1)
-            Application.LoadLevel(Application.loadedLevel + 1);
-        var tmp = Instantiate(constellations[currentIndex++]);
-        tmp.transform.position = SpawnPosition.position;
-        currentConstellation = tmp;
+            StartCoroutine(cycleConstellation());
+        }
+        private IEnumerator cycleConstellation()
+        {
+            score = 0;
+            Destroy(currentConstellation);
+            stars = null;
+            yield return new WaitForSeconds(3);
+            if (currentIndex == constellations.Length)
+            {
+                SceneManager.MoveGameObjectToScene(FindObjectOfType<Player>().gameObject, SceneManager.GetActiveScene());
+                Application.LoadLevel(Application.loadedLevel + 1);
+            }
 
+            var tmp = Instantiate(constellations[currentIndex++]);
+            tmp.transform.position = SpawnPosition.position;
+            currentConstellation = tmp;
+
+        }
     }
 }
