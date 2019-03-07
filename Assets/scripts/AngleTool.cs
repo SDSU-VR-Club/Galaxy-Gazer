4059d41bbf4fe12f8fe83c62f2445b7b8f220758
Carter <andrewscart@gmail.com>
2019-03-06 18:24:42 -0800

    cone projection
    changes to the angle tool to display a cone

--- Assets/scripts/AngleTool.cs
+++ Assets/scripts/AngleTool.cs
@@ -16,37 +16,53 @@ namespace Valve.VR.InteractionSystem
         Vector3 SecondAnglePosition;
         GameObject head;
         TextMeshProUGUI AngleUI;
-        
+        Mesh coneMesh;
         public int DecimalPlaces;
         bool started=false;
+        bool measuring = false;
+        Vector3[] vertices;
+        int[] triangles;
+        MeshRenderer renderer;
         // Use this for initialization
         void Start() {
             myHand = GetComponent<Hand>();
             green = new Color(0, 1, 0);
             head=FindObjectOfType<Camera>().gameObject;
             AngleUI = GetComponentInChildren<TextMeshProUGUI>();
+            coneMesh = new Mesh();
+            coneMesh.name = "coneMesh";
+            triangles = new int[6];
+            vertices = new Vector3[3];
+            renderer = GetComponent<MeshRenderer>();
+            GetComponent<MeshFilter>().mesh = coneMesh;
+            FirstAnglePosition = Vector3.right;
+            SecondAnglePosition = Vector3.forward;
+            renderer.enabled = false;
         }
        
        
         //Called on trigger press to select a star if pointed one is being pointed at
         void Update() {
+            
+            if (measuring)
+            {
+                SecondAnglePosition = shootTransform.forward;
+                drawCone();
+                float tempAngle = GetAngle(FirstAnglePosition, SecondAnglePosition);
+                if (tempAngle != -404)
+                    AngleUI.text = (Mathf.Round(tempAngle * Mathf.Pow(10, DecimalPlaces)) / Mathf.Pow(10, DecimalPlaces) + "°");
+            }
             RaycastHit hit;
+
             if (GetComponent<Hand>().grabPinchAction.GetLastStateDown(GetComponent<Hand>().handType))
             {
-
-                if (FirstAnglePosition == null)
-                {
-                    FirstAnglePosition = shootTransform.forward;
-                }
-                else
-                {
-                    SecondAnglePosition = FirstAnglePosition;
-                    FirstAnglePosition = shootTransform.forward;
-                    float tempAngle = GetAngle(FirstAnglePosition, SecondAnglePosition);
-                    if (tempAngle != -404)
-                        AngleUI.text = (Mathf.Round(tempAngle * Mathf.Pow(10, DecimalPlaces)) / Mathf.Pow(10, DecimalPlaces) + "°");
-
-                }
+                FirstAnglePosition = shootTransform.forward;
+                measuring = true;
+                renderer.enabled = true;
+            }
+            if (GetComponent<Hand>().grabPinchAction.GetLastStateUp(GetComponent<Hand>().handType)){
+                measuring = false;
+                renderer.enabled = false;
             }
            
 
@@ -57,6 +73,20 @@ namespace Valve.VR.InteractionSystem
         {
              return Vector3.Angle(first, second);
         }
+        void drawCone()
+        {
+            vertices[0] = shootTransform.position;
+            vertices[1] = shootTransform.position + FirstAnglePosition * 1000;
+            vertices[2] = shootTransform.position + SecondAnglePosition * 1000;
+            coneMesh.vertices = vertices;
+            triangles[0] = 0;
+            triangles[1] = 1;
+            triangles[2] = 2;
+            triangles[3] = 2;
+            triangles[4] = 1;
+            triangles[5] = 0;
+            coneMesh.triangles = triangles;
 
+        }
     }
 }
