a8867b26f79245ae17abd2dc4ad52e1b01af9754
petern-dev <nguyenp1997@me.com>
2019-03-27 13:43:08 -0700

    Updated OrbitTracker

--- Assets/scripts/OrbitTracker.cs
+++ Assets/scripts/OrbitTracker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OrbitTracker : MonoBehaviour
 {
@@ -8,14 +9,16 @@ public class OrbitTracker : MonoBehaviour
 
     private Vector3 previousPos;
     private float angle, totalAngle;
-    private int days, years;
+    private int days, years, months;
+    private TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
     {
         previousPos = Vector3.zero;
-        days = years = 0;
+        days = years = months = 0;
         angle = totalAngle = 0f;
+        text = textObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -24,17 +27,32 @@ public class OrbitTracker : MonoBehaviour
         angle = Vector3.Angle(previousPos, gameObject.transform.position);
         totalAngle += Mathf.Abs(angle);
 
-        if(totalAngle > 360f)
+        //months & years
+        months = Mathf.RoundToInt((totalAngle / 360) * 12);
+        if (months >= 12)
         {
             totalAngle -= 360;
             years++;
-            days -= 365;
+            months = 0;
         }
 
-        days = Mathf.RoundToInt((totalAngle / 360) * 365);
+        text.SetText("Time passed:\n{0} months, {1} years", months, years);
 
-        textObject.GetComponent<Text>().setText("Time passed:\n{0} days, {1} years", days, years);
-        Debug.Log(days + " days, " + years + " years");
+        // days version
+
+
+        //days = Mathf.RoundToInt((totalAngle / 360) * 365);
+        //if (days >= 365)
+        //{
+        //    totalAngle -= 360;
+        //    years++;
+        //    days = 0;
+        //}
+
+        //text.SetText("Time passed:\n{0} days, {1} years", days, years);
+
+        //Debug.Log(days + " days, " + years + " years");
+
         previousPos = gameObject.transform.position;
     }
 }
