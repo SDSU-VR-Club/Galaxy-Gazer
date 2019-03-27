06baf17fc724f0d95ef1484a1846aebc4216dbf3
petern-dev <nguyenp1997@me.com>
2019-03-27 13:11:17 -0700

    Day/year tracking added

---
+++ Assets/scripts/OrbitTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitTracker : MonoBehaviour
+{
+    public GameObject textObject;
+
+    private Vector3 previousPos;
+    private float angle, totalAngle;
+    private int days, years;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPos = Vector3.zero;
+        days = years = 0;
+        angle = totalAngle = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = Vector3.Angle(previousPos, gameObject.transform.position);
+        totalAngle += Mathf.Abs(angle);
+
+        if(totalAngle > 360f)
+        {
+            totalAngle -= 360;
+            years++;
+            days -= 365;
+        }
+
+        days = Mathf.RoundToInt((totalAngle / 360) * 365);
+
+        textObject.GetComponent<Text>().setText("Time passed:\n{0} days, {1} years", days, years);
+        Debug.Log(days + " days, " + years + " years");
+        previousPos = gameObject.transform.position;
+    }
+}
