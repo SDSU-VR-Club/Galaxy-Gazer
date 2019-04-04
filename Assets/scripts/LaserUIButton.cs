ddfd93824c0e9291bb904ac4ea196e5b84e7e78b
Kainkun <kainkun1000@gmail.com>
2019-04-04 16:26:00 -0700

    new Main menu scene called part0MainMenu

---
+++ Assets/scripts/LaserUIButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaserUIButton : MonoBehaviour
+{
+    [Header("Enable to 'enable objects' when the button is pressed")]
+    public bool EnableThings;
+    public GameObject[] ThingsToEnable;
+    [Header("Enable to 'disable objects' when the button is pressed")]
+    public bool DisableThings;
+    public GameObject[] ThingsToDisable;
+    [Header("Enable to go to a scene when the button is pressed")]
+    public bool GoToScene;
+    public string NextSceneName;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void Press()
+    {
+        if (GoToScene)
+            goToScene();
+
+        if (EnableThings)
+            enable();
+
+        if (DisableThings)
+            disable();
+
+    }
+
+    public void goToScene()
+    {
+        if(NextSceneName != null)
+        SceneManager.LoadScene(NextSceneName);
+    }
+
+    public void enable()
+    {
+        foreach (GameObject EnableObject  in ThingsToEnable)
+        {
+            EnableObject.SetActive(true);
+        }
+    }
+
+    public void disable()
+    {
+        foreach (GameObject EnableObject in ThingsToDisable)
+        {
+            EnableObject.SetActive(false);
+        }
+    }
+
+
+}
---
+++ Assets/scripts/UIPointing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem
+{
+    [RequireComponent(typeof(Hand))]
+    public class UIPointing : MonoBehaviour
+    {
+        Hand myHand;
+        RaycastHit hit;
+
+        void Start()
+        {
+            myHand = GetComponent<Hand>();
+        }
+
+        private void Update()
+        {
+            if (GetComponent<Hand>().grabPinchAction.GetLastStateDown(GetComponent<Hand>().handType))
+            {
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    if (hit.collider.gameObject.tag == "LaserUI")
+                    {
+
+                        hit.collider.GetComponent<LaserUIButton>().Press();
+
+                    }
+                }
+            }
+            
+        }
+
+
+
+
+    }
+}
