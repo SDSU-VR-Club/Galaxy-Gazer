74aea48ac42f507951b9754ad4e1ed4105393a0f
Carter <andrewscart@gmail.com>
2019-04-09 16:53:20 -0700

    skydraw

---
+++ Assets/scripts/skyDrawing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyDrawing : MonoBehaviour
+{
+    LineRenderer lr;
+    public float vertexInterval;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+    void createNewVert(Vector3 pos)
+    {
+        lr.positionCount = lr.positionCount + 1;
+        lr.SetPosition(lr.positionCount - 1, pos);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > vertexInterval)
+        {
+            timer = 0;
+            createNewVert(transform.position);
+        }
+    }
+}
