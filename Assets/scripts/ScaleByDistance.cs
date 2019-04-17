bb3ecca433cfaacaae28e2ce61e8addcaf0acef8
BuildIT Student Assistant <lib-buildit@sdsu.edu>
2019-04-17 15:53:21 -0700

    dded prefab file generation to randomStarGen

--- Assets/scripts/ScaleByDistance.cs
+++ Assets/scripts/ScaleByDistance.cs
@@ -7,13 +7,13 @@ public class ScaleByDistance : MonoBehaviour {
     public float scale;
     GameObject cam;
     float dist;
-
+    float initialScale;
     public GameObject hit;
 
 	// Use this for initialization
 	void Start () {
         cam = FindObjectOfType<Camera>().gameObject;
-
+        initialScale = transform.localScale.x;
        // GameObject x = Instantiate(hit, transform.position, Quaternion.identity);
        // x.transform.position -= new Vector3(0, 0, 1);
     }
@@ -22,7 +22,7 @@ public class ScaleByDistance : MonoBehaviour {
 	void Update () {
         dist = Vector3.Distance(gameObject.transform.position, cam.transform.position);
         float i = dist / Mathf.Pow(scale,-1) / 10;
-        transform.localScale = new Vector3(i, i, i);
+        transform.localScale = new Vector3(i, i, i)*initialScale;
         GetComponent<TrailRenderer>().startWidth = i;
     }
 }
